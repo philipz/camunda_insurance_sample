d9b72966faff85485ba39167d4ef91299ebff399
philipz <philipzheng@gmail.com>
2023-06-10 23:46:48 +0800

    Final state, add process test coverage.

---
+++ src/test/java/org/camunda/bpm/example/insuranceTest.java
@@ -0,0 +1,132 @@
+package org.camunda.bpm.example;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.withVariables;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Import(CoverageTestConfiguration.class)
+public class insuranceTest {
+
+        private static final String PROCESS_DEFINITION_KEY = "insurance";
+
+        @Autowired
+        public RuntimeService runtimeService;
+
+        private Map<String, Object> variables;
+
+        @Test
+        public void shouldExecuteHappyPath() {
+                // Define scenarios by using camunda-bpm-assert-scenario:
+                variables = Variables.createVariables();
+                variables.put("id", "id");
+                variables.put("name", "name");
+                variables.put("hasSocialSecurity", true);
+                variables.put("hasOtherInsurance", true);
+                ProcessInstance instance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY,
+                                variables);
+
+                assertThat(instance).isWaitingAt("IntermediateThrowEvent_ReceiveInsuranceContract");
+
+                assertThat(instance).variables().containsEntry("premium", 600L);
+
+                Execution execution = runtimeService.createExecutionQuery().processInstanceId(instance.getId())
+                                .messageEventSubscriptionName("Message_ReceiveInsuranceContract").singleResult();
+
+                runtimeService.messageEventReceived("Message_ReceiveInsuranceContract", execution.getId());
+
+                assertThat(instance)
+                                .hasPassed("EndEvent_InsuranceConfirmed")
+                                .isEnded();
+        }
+
+        @Test
+        public void shouldExecuteHappyPath1() {
+                // Define scenarios by using camunda-bpm-assert-scenario:
+                variables = Variables.createVariables();
+                variables.put("id", "id");
+                variables.put("name", "name");
+                variables.put("hasSocialSecurity", true);
+                variables.put("hasOtherInsurance", false);
+                ProcessInstance instance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY,
+                                variables);
+
+                assertThat(instance).isWaitingAt("Task_CheckInsuranceCase");
+
+                complete(task(), withVariables("isRiskManagable", true));
+
+                assertThat(instance).variables().containsEntry("premium", 1000L);
+
+                Execution execution = runtimeService.createExecutionQuery().processInstanceId(instance.getId())
+                                .messageEventSubscriptionName("Message_ReceiveInsuranceContract").singleResult();
+                runtimeService.messageEventReceived("Message_ReceiveInsuranceContract", execution.getId());
+
+                assertThat(instance)
+                                .hasPassed("EndEvent_InsuranceConfirmed")
+                                .isEnded();
+        }
+
+        @Test
+        public void missData() {
+                variables = Variables.createVariables();
+                variables.put("id", "id");
+                variables.put("hasSocialSecurity", false);
+                variables.put("hasOtherInsurance", true);
+                ProcessInstance instance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY,
+                                variables);
+
+                assertThat(instance)
+                                .hasPassed("EndEvent_NeedMoreDocument")
+                                .isEnded();
+        }
+
+        @Test
+        public void shouldRejectCondition1() {
+                variables = Variables.createVariables();
+                variables.put("id", "id");
+                variables.put("name", "name");
+                variables.put("hasSocialSecurity", false);
+                variables.put("hasOtherInsurance", true);
+                ProcessInstance instance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY,
+                                variables);
+
+                assertThat(instance)
+                                .hasPassed("EndEvent_InsuranceRejected_NotQualify")
+                                .isEnded();
+        }
+
+        @Test
+        public void shouldRejectCondition2() {
+                variables = Variables.createVariables();
+                variables.put("id", "id");
+                variables.put("name", "name");
+                variables.put("hasSocialSecurity", true);
+                variables.put("hasOtherInsurance", false);
+                ProcessInstance instance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY,
+                                variables);
+
+                assertThat(instance).isWaitingAt("Task_CheckInsuranceCase");
+
+                complete(task(), withVariables("isRiskManagable", false));
+
+                assertThat(instance)
+                                .hasPassed("EndEvent_InsuranceRejected_Risky")
+                                .isEnded();
+
+        }
+
+}
---
+++ src/test/java/org/camunda/bpm/example/rejectinsuranceTest.java
@@ -0,0 +1,34 @@
+package org.camunda.bpm.example;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Import(CoverageTestConfiguration.class)
+public class rejectinsuranceTest {
+
+        private static final String PROCESS_DEFINITION_KEY = "rejectInsurance";
+
+        @Autowired
+        public RuntimeService runtimeService;
+
+        @Test
+        public void shouldExecuteHappyPath() {
+
+                ProcessInstance instance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+                assertThat(instance)
+                                .hasPassed("EndEvent_InsuranceRejected")
+                                .isEnded();
+        }
+
+}
